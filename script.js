ae5bd05a5b14066f6607619dac9c9853815fb881
Larissa Faria <94909143+larissa-faria@users.noreply.github.com>
2022-03-23 16:04:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburguer").addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle("show_menu");//Se tiver a classe ele removera se não irá add // O container envolve td mundo. 
+})
